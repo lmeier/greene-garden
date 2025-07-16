450d1948e523df555dba1e0bd11bbe455be310c2
Liam Yates <liampyates@gmail.com>
2025-07-15 22:26:25 -0400

    Setup for Jekyll and Tailwind, skeleton of our pages

---
+++ blank-start/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+const typography = require('@tailwindcss/typography');
+
+module.exports = {
+  content: [
+    "./_layouts/**/*.html",
+    "./_includes/**/*.html",
+    "./*.html",
+    "./*.md",
+    "./*.markdown"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [typography],
+}
